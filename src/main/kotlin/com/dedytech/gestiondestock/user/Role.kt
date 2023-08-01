6eac84d6295d846a3387a15fe033ec500791242e
Your Name <you@example.com>
2023-08-01 09:51:30 +0300

    commit

---
+++ src/main/kotlin/com/dedytech/gestiondestock/user/Role.kt
@@ -0,0 +1,6 @@
+package com.dedytech.gestiondestock.user
+
+enum class Role {
+    USER,
+    AMIN
+}
--- src/main/kotlin/com/dedytech/gestiondestock/user/User.kt
+++ src/main/kotlin/com/dedytech/gestiondestock/user/User.kt
@@ -1,5 +1,88 @@
 package com.dedytech.gestiondestock.user
 
+import jakarta.persistence.*
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+@Entity
+@Table(name = "_user")
 data class User(
-    private Integer id
+    @Id
+    @GeneratedValue
+    var id: Int,
+    var firstname: String,
+    var lastname: String,
+    var email: String,
+    var password: String,
+
+    @Enumerated(EnumType.STRING)
+    val roles: List<Role> = listOf(Role.USER, Role.AMIN)
 )
+{
+    fun toUserDetails(): UserDetails {
+        val user = this@User
+        return object:UserDetails {
+
+            override fun getAuthorities() = user.roles.map { role ->
+                SimpleGrantedAuthority(role.name)
+            }.toList()
+
+            override fun getPassword(): String {
+                TODO("Not yet implemented")
+            }
+
+            override fun getUsername(): String {
+                TODO("Not yet implemented")
+            }
+
+            override fun isAccountNonExpired(): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun isAccountNonLocked(): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun isCredentialsNonExpired(): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun isEnabled(): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        }
+    }
+}
+
+//):UserDetails {
+//    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+//        TODO("Not yet implemented")
+//        return listOf(new SimpleGrantedAuthority(role.name()))
+//    }
+//
+//    override fun getPassword(): String {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getUsername(): String {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun isAccountNonExpired(): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun isAccountNonLocked(): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun isCredentialsNonExpired(): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun isEnabled(): Boolean {
+//        TODO("Not yet implemented")
+//    }
+//}
